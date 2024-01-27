e53a2748a1ee3f3f44011475420f18f311eb2bf5
Mike Back <hello@mikeback.me>
2024-01-27 23:50:31 +1100

    add calculator functions WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { numberKeys, operationKeys, equalsKey } from "./keyData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./styles/App.scss";
 
+type KeyType = "operation" | "number";
+
 function App() {
-  const [displayValue, setDisplayValue] = useState<number>(0);
+  const [displayValue, setDisplayValue] = useState<string>("0");
+  const [currentCalc, setCurrentCalc] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("new calc", currentCalc);
+  });
 
   function clearValue() {
-    setDisplayValue(0);
+    setDisplayValue("0");
+    setCurrentCalc([]);
+  }
+
+  function addToCalc(keyValue: string, keyType: KeyType) {
+    console.log("key clicked", keyValue, keyType);
+
+    if (currentCalc.length === 0 && keyType === "number") {
+      setCurrentCalc([...currentCalc, keyValue]);
+      setDisplayValue(keyValue);
+      return;
+    }
+    if (currentCalc.length === 0 && keyType === "operation") {
+      return;
+    }
+
+    if (currentCalc.length > 0) {
+      const previousEntry = currentCalc[currentCalc.length - 1];
+      const previousIsNumber = !["+", "-", "/", "*"].includes(previousEntry);
+
+      // It's a number (inc decimal point) and last was a number concatenate with last. update display
+      if (keyType === "number" && previousIsNumber) {
+        const copyCalc = [...currentCalc];
+        // prevent 2 leading zeros
+        let newNum = keyValue === "0" && previousEntry === "0" ? previousEntry : previousEntry + keyValue;
+        // TODO: prevent 2 decimal points
+        if (previousEntry.includes(".") && keyValue === ".") {
+          newNum = previousEntry;
+        }
+
+        copyCalc.splice(-1, 1, newNum);
+        setCurrentCalc(copyCalc);
+        setDisplayValue(newNum);
+        return;
+      }
+      // It's a number and last was an operation then add as a new entry in array. Update display
+      if (keyType === "number" && previousIsNumber === false) {
+        setCurrentCalc([...currentCalc, keyValue]);
+        setDisplayValue(keyValue);
+        return;
+      }
+
+      // It's an operation and last was a number then add as a new entry in array. Don't update display.
+      if (keyType === "operation" && previousIsNumber) {
+        setCurrentCalc([...currentCalc, keyValue]);
+        return;
+      }
+      // if it's a operator and last one was also an operator the overwrite it instead. Don't update display.
+      if (keyType === "operation" && previousIsNumber === false) {
+        const copyCalc = [...currentCalc];
+        keyValue === "-" ? copyCalc.push(keyValue) : copyCalc.splice(-1, 1, keyValue);
+        setCurrentCalc(copyCalc);
+        return;
+      }
+    }
+
+    console.log("ERROR - THIS SHOULD BE UNREACHABLE CODE");
+  }
+
+  function calculateAnswer() {
+    let answer = eval(currentCalc.join(""));
+
+    if (Math.floor(answer) !== answer) {
+      answer = answer.toFixed(8);
+    }
+    setDisplayValue(answer + "");
+    setCurrentCalc([answer + ""]);
   }
 
   return (
@@ -18,8 +91,12 @@ function App() {
 
       <div id="operation-keys">
         {operationKeys.map((opKey) => {
+          function processClick() {
+            addToCalc(opKey.value, "operation");
+          }
+
           return (
-            <div id={opKey.id} className="key">
+            <div id={opKey.id} className="key" onClick={processClick} key={opKey.id}>
               <FontAwesomeIcon icon={opKey.icon} />
             </div>
           );
@@ -28,8 +105,12 @@ function App() {
 
       <div id="number-keys">
         {numberKeys.map((numKey) => {
+          function processClick() {
+            addToCalc(numKey.value, "number");
+          }
+
           return (
-            <div id={numKey.id} className="key">
+            <div id={numKey.id} className="key" onClick={processClick} key={numKey.id}>
               <FontAwesomeIcon icon={numKey.icon} />
             </div>
           );
@@ -40,7 +121,7 @@ function App() {
         <div id="clear" className="key" onClick={clearValue}>
           clear
         </div>
-        <div id="equals" className="key">
+        <div id="equals" className="key" onClick={calculateAnswer}>
           <FontAwesomeIcon icon={equalsKey.icon} />
         </div>
       </div>
--- src/keyData.ts
+++ src/keyData.ts
@@ -3,7 +3,7 @@ import { fa0, fa1, fa2, fa3, fa4, fa5, fa6, fa7, fa8, fa9 } from "@fortawesome/f
 import { faPlus, faMinus, faMultiply, faDivide, faEquals, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 type Key = {
-  value: string | number;
+  value: string;
   id: string;
   icon: IconProp;
 };
@@ -11,22 +11,22 @@ type Key = {
 export const operationKeys: Key[] = [
   { value: "+", id: "add", icon: faPlus },
   { value: "-", id: "subtract", icon: faMinus },
-  { value: "X", id: "multiply", icon: faMultiply },
+  { value: "*", id: "multiply", icon: faMultiply },
   { value: "/", id: "divide", icon: faDivide },
 ];
 
 export const equalsKey: Key = { value: "=", id: "equals", icon: faEquals };
 
 export const numberKeys: Key[] = [
-  { value: 1, id: "one", icon: fa1 },
-  { value: 2, id: "two", icon: fa2 },
-  { value: 3, id: "three", icon: fa3 },
-  { value: 4, id: "four", icon: fa4 },
-  { value: 5, id: "five", icon: fa5 },
-  { value: 6, id: "six", icon: fa6 },
-  { value: 7, id: "seven", icon: fa7 },
-  { value: 8, id: "eight", icon: fa8 },
-  { value: 9, id: "nine", icon: fa9 },
-  { value: 0, id: "zero", icon: fa0 },
+  { value: "1", id: "one", icon: fa1 },
+  { value: "2", id: "two", icon: fa2 },
+  { value: "3", id: "three", icon: fa3 },
+  { value: "4", id: "four", icon: fa4 },
+  { value: "5", id: "five", icon: fa5 },
+  { value: "6", id: "six", icon: fa6 },
+  { value: "7", id: "seven", icon: fa7 },
+  { value: "8", id: "eight", icon: fa8 },
+  { value: "9", id: "nine", icon: fa9 },
+  { value: "0", id: "zero", icon: fa0 },
   { value: ".", id: "decimal", icon: faCircle },
 ];
